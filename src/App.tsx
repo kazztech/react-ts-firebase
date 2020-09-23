9bd2fe0f4370a327bce8c765a7ec81b9b358ad2a
kazz <kazzzz1001@gmail.com>
2020-09-23 12:07:29 +0900

    ダミーデータでの表示を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  height: number;
+};
 
 function App() {
-  return <div></div>;
+  const [users, setUsers] = useState<User[]>([
+    { id: "ID1", name: "ゾンビ", height: 195 },
+    { id: "ID2", name: "スケルトン", height: 199 },
+    { id: "ID3", name: "クリーパー", height: 170 },
+  ]);
+  return (
+    <div>
+      <h2>Users</h2>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.name} ({user.height}cm)
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
